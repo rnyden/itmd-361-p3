71870d37e50ba5f3019de5cd0063219d50468ba1
rnyden <rnyden@hawk.iit.edu>
2022-04-13 02:51:48 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 var platform = new H.service.Platform({
   'apikey': '{OpslUQ2FwfE_6whPEWSaLDDMl2Z7A_AP7lb9QSw73Y4}'
 });
-var defaultMapTypes = platform.createDefaultLayers();
-var myMap = new H.Map(document.getElementById('mapContainer'),
-  defaultMapTypes.vector.normal.map,
+var defaultLayers = platform.createDefaultLayers();
+var map = new H.Map(document.getElementById('mapContainer'),
+  defaultLayers.vector.normal.map,
   {
     zoom: 10,
     center: { lat: 52.5, lng: 13.4 }
   });
 
-google.maps.event.addDomListener(window, 'load', initMap);
+/*google.maps.event.addDomListener(window, 'load', initMap);*/
