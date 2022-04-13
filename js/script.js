d806450cb19134a0a06cd212c49ba5b3fea28a1d
rnyden <rnyden@hawk.iit.edu>
2022-04-13 02:43:16 -0500

    display map

--- js/script.js
+++ js/script.js
@@ -2,16 +2,11 @@ var platform = new H.service.Platform({
   'apikey': '{OpslUQ2FwfE_6whPEWSaLDDMl2Z7A_AP7lb9QSw73Y4}'
 });
 var defaultMapTypes = platform.createDefaultLayers();
+var myMap = new H.Map(document.getElementById('mapContainer'),
+  defaultMapTypes.vector.normal.map,
+  {
+    zoom: 10,
+    center: { lat: 52.5, lng: 13.4 }
+  });
 
-function initMap() {
-  const place = { lat: -25.344, lng: 131.036 };
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 3,
-    center: place,
-  });
-  const marker = new google.maps.Marker({
-    position: place,
-    map: map,
-  });
-}
 google.maps.event.addDomListener(window, 'load', initMap);
