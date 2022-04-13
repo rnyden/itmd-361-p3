059eba200591a858dfe1a32a0b0519171ca9a6ad
rnyden <rnyden@hawk.iit.edu>
2022-04-13 02:35:24 -0500

    connecting to back-end services

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+var platform = new H.service.Platform({
+  'apikey': '{OpslUQ2FwfE_6whPEWSaLDDMl2Z7A_AP7lb9QSw73Y4}'
+});
+
 function initMap() {
   const place = { lat: -25.344, lng: 131.036 };
   const map = new google.maps.Map(document.getElementById("map"), {
