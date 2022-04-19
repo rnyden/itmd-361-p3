920313b6ae1186b4fb7c0a7765154f2b93b0f9a9
rnyden <rnyden@hawk.iit.edu>
2022-04-19 17:40:03 -0500

    added a marker on the map

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.414170, lng: -3.707970},
+	const casaBotin = { lat: 40.414170, lng: -3.707970 };
+  const map = new google.maps.Map(document.getElementById('map'), {
+    center: casaBotin,
     zoom: 10
   });
- 
+	const marker1 = new google.maps.Marker({
+    position: casaBotin,
+    map: map,
+  });
+
 }
 google.maps.event.addDomListener(window, 'load', initMap);
