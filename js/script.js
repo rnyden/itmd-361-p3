ae91b2c0c73c7941a03dc217199fa30c31fde48e
rnyden <rnyden@hawk.iit.edu>
2022-04-19 01:49:09 -0500

    map now works

--- js/script.js
+++ js/script.js
@@ -1,14 +1,9 @@
-var platform = new H.service.Platform({
-	appid: 'n9BhwoF43uZ3bLxqDfaw'
-  apikey: 'OpslUQ2FwfE_6whPEWSaLDDMl2Z7A_AP7lb9QSw73Y4'
-});
-var defaultLayers = platform.createDefaultLayers();
 var map;
 function initMap() {
-	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: -34.397, lng: 150.644},
-		zoom: 8
-	});
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+ 
 }
-
-/*google.maps.event.addDomListener(window, 'load', initMap);*/
+google.maps.event.addDomListener(window, 'load', initMap);
