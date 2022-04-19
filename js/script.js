2b17ed6e31345cfca23c8aa38b6340ef8f0ddb96
rnyden <rnyden@hawk.iit.edu>
2022-04-19 18:11:17 -0500

    added popup info when user interacts with map marker

--- js/script.js
+++ js/script.js
@@ -5,9 +5,32 @@ function initMap() {
     center: casaBotin,
     zoom: 10
   });
+	const aboutRestaurant =
+    '<div id="content">' +
+    '<div id="siteNotice">' +
+    "</div>" +
+    '<h2 id="firstHeading" class="firstHeading">Sobrino de Botín</h2>' +
+    '<div id="bodyContent">' +
+    "<p><strong>Sobrino de Botín</strong>, also known as <strong>Casa Botín</strong>, was founded in 1725 and is the world''s oldest " +
+    "restaurant.</p>" +
+    '<p>Attribution: <a href="https://botin.es/en/home/">Sobrino de Botín</a>' +
+    "</p>" +
+    "</div>" +
+    "</div>";
+  const infowindow = new google.maps.InfoWindow({
+    content: aboutRestaurant,
+  });
 	const marker1 = new google.maps.Marker({
     position: casaBotin,
     map: map,
+		title: "Sobrino de Botín",
+  });
+	marker1.addListener("click", () => {
+    infowindow.open({
+      anchor: marker1,
+      map,
+      shouldFocus: false,
+    });
   });
 
 }
