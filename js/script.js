05d887fc48f59e86210ca719bb7852cdc2eb5464
rnyden <rnyden@hawk.iit.edu>
2022-04-19 17:25:29 -0500

    center location at Sobrino de Botin

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
+    center: {lat: 40.414170, lng: -3.707970},
     zoom: 8
   });
  
